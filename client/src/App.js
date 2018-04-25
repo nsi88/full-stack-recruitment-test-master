26ba9219c39906dcd95d4329f051a0258040bfbe
Sergey Novikov <novikovseregka@gmail.com>
2018-04-25 18:27:10 +0800

    Take results from state

--- client/src/App.js
+++ client/src/App.js
@@ -8,145 +8,24 @@ import Results from './components/results';
 
 import dateTimeHelper from './date-time-helper.js';
 
-const fromPlace = 'EDI', toPlace = 'LON', adults = 2, cabinClass = 'Economy',
+const fromPlace = 'EDI-sky', toPlace = 'LOND-sky', adults = 2, cabinClass = 'Economy',
   fromDate = dateTimeHelper.nextMonday(), toDate = dateTimeHelper.folowingDay(fromDate);
 
-const results = [{
-  "Agent": {
-      "Name": "omegaflightstore.com"
-  },
-  "Currency": {
-      "Symbol": "\u00a3"
-  },
-  "Legs": [
-      {
-          "Arrival": "2018-04-30T08:30:00",
-          "Carrier": {
-              "Code": "EZ",
-              "Name": "Brussels Airlines"
-          },
-          "Departure": "2018-04-30T07:00:00",
-          "DestinationStation": {
-              "Code": "LHR"
-          },
-          "Duration": 90,
-          "OriginStation": {
-              "Code": "EDI"
-          },
-          "StopsCount": 0
-      },
-      {
-          "Arrival": "2018-05-01T16:00:00",
-          "Carrier": {
-              "Code": "EZ",
-              "Name": "British Airways"
-          },
-          "Departure": "2018-05-01T14:30:00",
-          "DestinationStation": {
-              "Code": "EDI"
-          },
-          "Duration": 90,
-          "OriginStation": {
-              "Code": "LHR"
-          },
-          "StopsCount": 0
-      }
-  ],
-  "Price": 98
-}, {
-  "Agent": {
-      "Name": "omegaflightstore.com"
-  },
-  "Currency": {
-      "Symbol": "\u00a3"
-  },
-  "Legs": [
-      {
-          "Arrival": "2018-04-30T08:30:00",
-          "Carrier": {
-              "Code": "EZ",
-              "Name": "Brussels Airlines"
-          },
-          "Departure": "2018-04-30T07:00:00",
-          "DestinationStation": {
-              "Code": "LHR"
-          },
-          "Duration": 90,
-          "OriginStation": {
-              "Code": "EDI"
-          },
-          "StopsCount": 0
-      },
-      {
-          "Arrival": "2018-05-01T16:00:00",
-          "Carrier": {
-              "Code": "EZ",
-              "Name": "British Airways"
-          },
-          "Departure": "2018-05-01T14:30:00",
-          "DestinationStation": {
-              "Code": "EDI"
-          },
-          "Duration": 90,
-          "OriginStation": {
-              "Code": "LHR"
-          },
-          "StopsCount": 0
-      }
-  ],
-  "Price": 98
-}, {
-  "Agent": {
-      "Name": "omegaflightstore.com"
-  },
-  "Currency": {
-      "Symbol": "\u00a3"
-  },
-  "Legs": [
-      {
-          "Arrival": "2018-04-30T08:30:00",
-          "Carrier": {
-              "Code": "EZ",
-              "Name": "Brussels Airlines"
-          },
-          "Departure": "2018-04-30T07:00:00",
-          "DestinationStation": {
-              "Code": "LHR"
-          },
-          "Duration": 90,
-          "OriginStation": {
-              "Code": "EDI"
-          },
-          "StopsCount": 0
-      },
-      {
-          "Arrival": "2018-05-01T16:00:00",
-          "Carrier": {
-              "Code": "EZ",
-              "Name": "British Airways"
-          },
-          "Departure": "2018-05-01T14:30:00",
-          "DestinationStation": {
-              "Code": "EDI"
-          },
-          "Duration": 90,
-          "OriginStation": {
-              "Code": "LHR"
-          },
-          "StopsCount": 0
-      }
-  ],
-  "Price": 98
-}];
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: []
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <TopNav/>
         <Header fromPlace={fromPlace} toPlace={toPlace} adults={adults} cabinClass={cabinClass}/>
         <Controls/>
-        <Results results={results}/>
+        <Results results={this.state.results}/>
       </div>
     );
   }
